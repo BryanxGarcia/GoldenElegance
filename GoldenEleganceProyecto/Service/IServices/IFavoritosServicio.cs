ace13d752d898443c6c06b8b33557341e6fe93a2
AntonioLH-Caamal <caamaljesus0@gmial.com>
2023-06-29 18:29:38 -0500

    EC-72 Creacion de los servicios de favorito

---
+++ GoldenEleganceProyecto/Service/IServices/IFavoritosServicio.cs
@@ -0,0 +1,12 @@
+﻿using GoldenEleganceProyecto.Models;
+using GoldenEleganceProyecto.Models.Helpers;
+
+namespace GoldenEleganceProyecto.Service.IServices
+{
+    public interface IFavoritosServicio
+    {
+        Task<List<Favoritos>> ObtenerLista(int? Id);
+        Task<ResponseHelper> AgregarFavorito(Favoritos vm);
+        Task<ResponseHelper> EliminarFavorito(int? Id);
+    }
+}
