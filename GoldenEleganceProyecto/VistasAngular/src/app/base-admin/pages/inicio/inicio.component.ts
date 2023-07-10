f787a3ec58fed19d0dd74d004d7d78fe92d7e0b9
Cristopher Bryan Bautista Garcia <92136012+BryanxGarcia@users.noreply.github.com>
2023-07-09 19:22:09 -0500

    EC-73 filtros de busqueda de los productos

--- GoldenEleganceProyecto/VistasAngular/src/app/base-admin/pages/inicio/inicio.component.ts
+++ GoldenEleganceProyecto/VistasAngular/src/app/base-admin/pages/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategoria } from 'src/app/models/ICategoria.interface';
 import { IProductos } from 'src/app/models/IProductos.interface.';
+import { CategoriaService } from 'src/app/services/categoria/categoria.service';
 import { ProductsService } from 'src/app/services/productsService/products.service';
 
 @Component({
@@ -7,21 +9,67 @@ import { ProductsService } from 'src/app/services/productsService/products.servi
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent implements OnInit{
-
-productos:IProductos[] =[];
-
-  constructor(private _productsService: ProductsService){
-
-  }
-
+export class InicioComponent implements OnInit {
+  constructor(private _productsService: ProductsService, private catServicio: CategoriaService) { }
+  Categorias: ICategoria[]=[];
+  productos: IProductos[] = [];
+  priceMin= 0;
+  priceMax = 1000;
+  searchTerm= '';
+  categoriaSelec=0;
   ngOnInit(): void {
     this.obtenerProductos();
+    this.obtenerCategorias();
   }
 
+  filteredProducts:IProductos[]= [];
+
+  updatePriceMin(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.priceMin =  parseFloat(target.value);
+    this.applyFilters();
+  }
+
+  updatePriceMax(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.priceMax =  parseFloat(target.value);
+    this.applyFilters();
+  }
+
+  buscarNombre(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.searchTerm =  target.value;
+    this.applyFilters();
+  }
+  onCategoriaSelected(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.categoriaSelec =  parseFloat(target.value);
+    this.applyFilters();
+  }
+  applyFilters() {
+    this.filteredProducts = this.productos.filter(product => {
+      const matchesPriceMin = isNaN(this.priceMin) || product.precioVenta >= this.priceMin;
+      const matchesPriceMax = isNaN(this.priceMax) || product.precioVenta <= this.priceMax;
+      const matchesSearchTerm = this.searchTerm.trim() === '' || product.nombreProducto.toLowerCase().includes(this.searchTerm.toLowerCase());
+      const matchesCat = isNaN(this.categoriaSelec) || product.fkCategoria == this.categoriaSelec;
+
+      return matchesPriceMin && matchesPriceMax && matchesSearchTerm && matchesCat;
+    });
+  }
   obtenerProductos() {
     this._productsService.listarProducto().subscribe(data => {
       this.productos = data;
+      this.filteredProducts = data;
     })
+  }
+  obtenerCategorias() {
+    this.catServicio.listarCategorias().subscribe(
+      (response) => {
+        this.Categorias = response;
+      },
+      (error) => {
+        console.error('Error al obtener productos:', error);
+      }
+    );
   }
 }
